d7d5fe655a9af70b686178b6477550e1a58cd77f
dragondungeon1 <lennartderidder@gmail.com>
2021-07-07 22:02:13 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,63 @@
+export const largeCardSections = [
+  {
+    id: 1,
+    title: "Services",
+    snippet: "DeveloKey is optimized for your online whises.",
+    cards: [
+      {
+        id: 19,
+        title: "Webdevelopment",
+        snippet: "DeveloKey will offer you a full working and responsive",
+        image: "web-development.svg"
+      },
+      {
+        id: 20,
+        title: "Webdesign",
+        snippet:
+          "Do you just need a design? DeveloKey will create a fresh design and a prototype",
+        image: "web-design.svg"
+      },
+      {
+        id: 21,
+        title: "Custom webapplications",
+        snippet:
+          "Do you need a specific application? No worries let's put our heads together",
+        image: "web-development.svg"
+      }
+    ]
+  }
+];
+
+export const smallCardSections = [
+  {
+    id: 1,
+    title: "Worked Projects",
+    cards: [
+      {
+        id: 1,
+        title: "BackstageMarketing",
+        snippet:
+          "Do you need a specific application? No worries let's put our heads together",
+      },
+      {
+        id: 2,
+        title: "Mr.Network",
+        snippet:
+          "Do you need a specific application? No worries let's put our heads together",
+      },
+      {
+        id: 3,
+        title: "PreventPlus",
+        snippet:
+          "Do you need a specific application? No worries let's put our heads together",
+      },
+      {
+        id: 4,
+        title: "The PhoneAmbulance",
+        snippet:
+          "Do you need a specific application? No worries let's put our heads together",
+      }
+    ]
+  },
+  
+];
